9edb5a3a87a99990555ef2b44d1a936d11320ae9
Andie Nare <andienare@gmail.com>
2021-02-14 18:54:57 -0500

    StateMachine

--- FSM/EcsStateMachine.cs
+++ FSM/EcsStateMachine.cs
@@ -1,4 +1,30 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿using System;
+
+namespace MelonECS.FSM
 {
-  public class $CLASS$ {$END$}
+    public class EcsStateMachine<TEnum> : System, IStateMachine<TEnum> where TEnum : Enum
+    {
+        private readonly StateMachine<TEnum> stateMachine = new StateMachine<TEnum>();
+        
+        public override void Run()
+        {
+            stateMachine.Update();
+        }
+
+        public IStateMachine<TEnum> AddState<TState>(TEnum stateEnum) where TState : IState, new()
+        {
+            stateMachine.AddState<TState>(stateEnum);
+            return this;
+        }
+
+        public void ChangeState(TEnum stateEnum)
+        {
+            stateMachine.ChangeState(stateEnum);
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- FSM/StateMachine.cs
+++ FSM/StateMachine.cs
@@ -1,7 +1,50 @@
-﻿namespace MelonECS.FSM
+﻿using System;
+using System.Collections.Generic;
+
+namespace MelonECS.FSM
 {
-    public class FSM
+    public interface IState
     {
+        void OnEnter();
+        void OnExit();
+        void Update();
+    }
+    
+    public interface IStateMachine<TEnum> where TEnum : Enum
+    {
+        IStateMachine<TEnum> AddState<TState>(TEnum stateEnum) where TState : IState, new();
+        void ChangeState(TEnum stateEnum);
+        void Update();
+    }
+    
+    public class StateMachine<TEnum> : IStateMachine<TEnum> where TEnum : Enum
+    {
+        private readonly Dictionary<TEnum, IState> states = new Dictionary<TEnum, IState>();
+        private IState currentState;
+        private IState pendingState;
         
+        public IStateMachine<TEnum> AddState<TState>(TEnum stateEnum) where TState : IState, new()
+        {
+            states.Add(stateEnum, new TState());
+            return this;
+        }
+
+        public void ChangeState(TEnum stateEnum)
+        {
+            pendingState = states[stateEnum];
+        }
+
+        public void Update()
+        {
+            currentState?.Update();
+
+            if (pendingState != null)
+            {
+                currentState?.OnExit();
+                currentState = pendingState;
+                currentState?.OnEnter();
+                pendingState = null;
+            }
+        }
     }
 }
